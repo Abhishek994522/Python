cd3d426b77af123e6625f4b72972e082f60cf3a9
ABHISHEK DODDAMANI <140547445+Abhishek994522@users.noreply.github.com>
2026-01-03 14:07:44 +0000

    commit

---
+++ one.py
@@ -0,0 +1,174 @@
+
+'''
+print("hello")
+
+# variables 
+# variables are containers which are used to store a data 
+
+a = 10;
+b = "44";
+
+print(a)
+print(b)
+
+print(type(a))
+print(type(b))
+
+#Datatypes in python 
+
+Data Types in Python
+Python supports several built-in data types:
+• 
+• 
+Integers (
+int ): Whole numbers (e.g., 
+Floats (
+10 , 
+float ): Decimal numbers (e.g., -5 ).
+3.14 , 
+• 
+Strings (-0.001 ).
+str ): Text data enclosed in quotes (e.g., 
+• 
+• 
+Booleans (
+bool ): Represents 
+True or 
+False .
+Lists: Ordered, mutable collections (e.g., 
+"Hello" , 
+[1, 2, 3] ).
+• 
+• 
+Tuples: Ordered, immutable collections (e.g., 
+(1, 2, 3) ).
+Sets: Unordered collections of unique elements (e.g., 
+'Python' ).
+{1, 2, 3} ).
+• 
+Dictionaries: Key-value pairs (e.g., 
+Checking Data Types
+
+
+
+#typecasting in python 
+
+c = str(a)
+d = int(b)
+
+print(type(c))
+print(type(d))
+
+# taking input from the user
+
+ab = input("enter a number")
+print(ab)
+
+ac = input("enter a number")
+ac = int(ac)
+print(ac+4)
+
+
+ad = int(input("enter a number"))
+ae = int(input("enter another number"))
+print(ad+ae)
+
+'''
+''' 
+# escape sequences
+
+print("hello \n iam good")
+
+print("hi iam \t manvi")
+
+print("hi iam \\ abcd123")
+
+print("hi iam \' abcd123")
+
+# print statements
+
+print('Hello world',"hello manvi",5, sep=",") 
+print('hi iam manvi', end=",")
+print('iam a youtuber')
+
+
+
+Types of Operators
+1. 
+Arithmetic Operators: 
+1. 
++ (Addition), - (Subtraction), 
+* (Multiplication), 
+/ (Division), 
+(Modulus), 
+2. 
+Example:
+** (Exponentiation), 
+// (Floor Division).
+print(10 + 5) # Output: 15
+print(10 ** 2) # Output: 100
+%
+2. 
+3. 
+Comparison Operators: 
+1. 
+2. 
+== (Equal), 
+!= (Not Equal), 
+(Greater Than or Equal), 
+Example:
+> (Greater Than), 
+< (Less Than), 
+<= (Less Than or Equal).
+print(10 > 5) # Output: True
+print(10 == 5) # Output: False
+>=
+Logical Operators: 
+1. 
+2. 
+and , 
+or , 
+Example:
+not .
+print(True and False) # Output: False
+print(True or False) # Output: True
+print(not True)
+# Output: False
+Assignment Operators: 
+4. 
+5. 
+6. 
+1. 
+2. 
+= , 
++= , -= , 
+Example:
+*= , 
+/= , 
+%= , 
+**= , 
+//= .
+x = 10
+x += 5 # Equivalent to x = x + 5
+print(x) # Output: 15
+Membership Operators: 
+1. 
+2. 
+in , 
+not in .
+Example:
+fruits = ["apple", "banana", "cherry"]
+print("banana" in fruits) # Output: True
+Identity Operators: 
+1. 
+2. 
+is , 
+is not .
+Example:
+x = 10
+y = 10
+print(x is y) # Output: True
+
+'''
+
+
---
+++ practice_set1.py
@@ -0,0 +1,52 @@
+
+# print("Hello World!, Welcome to Python")
+
+# print("Twinkle twinkle little star, \n How i wonder what you are!")
+
+# # name = input("enter your name")
+# # age = int(input("enter your age"))
+# # height = float(input("enter your height"))
+# # is_student = bool(input("is_student"))
+
+# # print(name,age,height,is_student, sep=",")
+
+# a = "abhi"
+# age = 23
+# height = 1.45
+# is_student = True
+
+# print(a,age,height,is_student, sep=",")
+# print("the name of the student is "+ a + " he is " + str(age) + " years old " + " and his height is " + str(height) +" meters " + "Student: "+str(is_student));
+
+
+# num = "45"
+# num = int(num)
+# print(num+10)
+
+
+# num = "45"
+# print(int(num)+10)
+
+# food = input("enter your favorite food?")
+# print("wow! i also like ",food)
+
+# simpe calculator
+
+# num1 = int(input("Enter first number"))
+# num2 = int(input("Enter second number"))
+
+# print(num1+num2)
+# print(num2-num1)
+# print(num1*num2)
+# print(num2/num1)
+
+
+#escape sequence 
+
+# print("Harry Said, \"python is Awsome!\" \n this is on a new line. \n This is a Tab->\t<-here")
+
+#operator challenge
+
+# a = int(input("enter a number"))
+# print(a*a)
+# print(a*a*a)
